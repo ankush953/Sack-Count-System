9bd40ab57d1321b26d569d72c4011f45acdfaf34
ankush953 <ankushraghuvanshi07@gmail.com>
2019-07-30 11:56:04 +0530

    requirements.txt

--- sacks/views.py
+++ sacks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from companies.models import Company
+import datetime
+from sacks.models import SackLoading, SackUnloading
 # Create your views here.
 def home(request):
 	qs = Company.objects.all()
@@ -18,11 +20,18 @@ def query(request):
 	startdate = request.POST['startdate']
 	enddate = request.POST['enddate']
 	company = request.POST['company']
-	print(startdate,enddate, company)
-	
+	# print(startdate,enddate, company)
+	startdate = datetime.datetime.strptime(startdate,'%m/%d/%Y')
+	enddate = datetime.datetime.strptime(enddate,'%m/%d/%Y')
+
+	lqs = SackLoading.objects.filter(loading_date_time__gte=startdate,loading_date_time__lte=enddate)
+	uqs = SackUnloading.objects.filter(unloading_date_time__gte=startdate,unloading_date_time__lte=enddate)
+
 	context = {
 		'startdate':startdate,
 		'enddate':enddate,
+		'lqs':lqs,
+		'uqs':uqs,
 		'company':company,
 		'title':'Query',
 	}
